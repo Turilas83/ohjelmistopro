5413035e459c6e2e568af1ce9b586d00fb1aa5a9
Mark Laatikainen <mark.laatikainen@gmail.com>
2017-12-04 09:05:23 +0200

    toimii taas

--- src/main/java/base/controller/AdminController.java
+++ src/main/java/base/controller/AdminController.java
@@ -4,15 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 @CrossOrigin
 @Controller
 public class AdminController {
 
 	@RequestMapping(value = "/admin", method = RequestMethod.GET)
-	public ModelAndView main() {
-		ModelAndView modelAndview = new ModelAndView("secure/admin");
-		return modelAndview;
+	public String main() {
+		return "secure/admin";
 	}
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String hello() {
+		return "index";
+	}
+
 }
